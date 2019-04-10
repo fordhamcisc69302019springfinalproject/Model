4b3b6cc9703193757c560e5de9fa8e658ec1e54e
fishercht1995 <fuyuqi1995@gmail.com>
2019-04-09 21:17:11 -0400

    add svm,knn and project_design

--- Predictor.py
+++
@@ -1,9 +0,0 @@
-import Simple_preprocess
-train_data, train_label, test_data, test_label = Simple_preprocess.preprocess()
-
-def predict(classifier,test = True):
-    classifier.fit(train_data,train_label)
-    if test:
-        return classifier.predict(test_data)
-    else:
-        return classifier.predict(train_data)
